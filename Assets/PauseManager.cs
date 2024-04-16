29013f7db726595577e7423a513c305f4c809c31
WWalter <walter.wanerskog2@gmail.com>
2024-04-16 11:03:35 +0200

    comit pause anager

--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -1,23 +1,46 @@
 using UnityEngine;
 
-public class PauseManager : MonoBehaviour {
-    private bool isPaused = false;
+public class PauseManager 
+    : MonoBehaviour {
 
+    public Canvas canvas;
+    public Canvas canvas2;
+    private bool funFactor = false;
+    private bool light = false;
+    private bool isPaused = false;
+    void Start () {
+        canvas.enabled = isPaused;
+        canvas2.enabled = isPaused;
+    }
     void Update() {
         if (Input.GetKeyDown(KeyCode.P)) {
             if (isPaused)
-                ResumeGame();            
-            else            
-                PauseGame();            
+                ResumeGame();
+            else
+                PauseGame();
+        }
+        if (isPaused && funFactor)
+        {
+            canvas2.enabled = light;
+            if (light)            
+                light = false;            
+            else 
+                light = true;
         }
     }
 
     void PauseGame() {
         Time.timeScale = 0;
-        isPaused = true;        
+        isPaused = true;
+        canvas.enabled = true;
+        if (Random.Range(0, 20) == 1) 
+            funFactor = true;
     }
     void ResumeGame() {
         Time.timeScale = 1;
-        isPaused = false;        
+        isPaused = false;
+        canvas.enabled = false;
+        canvas2.enabled = false;
+        funFactor = false;
     }
 }

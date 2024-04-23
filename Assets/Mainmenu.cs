7118152d8658519e1bfb8d9a6921fe6f2723d5bd
EnderMenderMan <93773937+EnderMenderMan@users.noreply.github.com>
2024-04-23 10:32:37 +0200

    change starter stats

--- Assets/Mainmenu.cs
+++ Assets/Mainmenu.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Mainmenu : MonoBehaviour
+public class Mainmenu : MonoBehaviour, IDataPersitiens
 {
+    int sceneIndex;
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(sceneIndex);
     }
 
     public void QuitGame()
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void LoadData(GameData data)
+    {
+        sceneIndex = data.sceneIndex;
+    }
+    public void SaveData(ref GameData data)
+    {
+
     }
 }
